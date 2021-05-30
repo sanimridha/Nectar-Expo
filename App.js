8b0591804017f581487edc7355e2bfd6eeb5f85e
sanimridha <sanimridha307@gmail.com>
2021-05-30 13:07:58 +0600

    added images constants

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { images } from "./app/constants";
 
 export default function App() {
     return (
         <View style={styles.container}>
             <Text>Open up App.js to start working on your --app!</Text>
             <StatusBar style="auto" />
+            <Image source={images.fruitsBag} />
         </View>
     );
 }
---
+++ app/constants/images.js
@@ -0,0 +1,19 @@
+const accepted = require("../../assets/photos/accepted.png");
+const bag = require("../../assets/photos/bag.png");
+const bottomBlur = require("../../assets/photos/bottomBlur.png");
+const fruitsBag = require("../../assets/photos/fruitsBag.png");
+const LocationImage = require("../../assets/photos/LocationImage.png");
+const Onboarding = require("../../assets/photos/Onboarding.png");
+const topBlur = require("../../assets/photos/topBlur.png");
+const topIcon = require("../../assets/photos/topIcon.png");
+
+export default {
+    accepted,
+    bag,
+    bottomBlur,
+    fruitsBag,
+    LocationImage,
+    Onboarding,
+    topBlur,
+    topIcon,
+};
