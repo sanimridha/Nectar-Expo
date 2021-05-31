a836e583e67d09f168722f2f819ab28ab8517b54
sanimridha <sanimridha307@gmail.com>
2021-05-31 09:40:44 +0600

    Expo before Ejection

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { images } from "./app/constants";
 import Onboarding from "./app/Screens/Onboarding";
+import SigninScreen from "./app/Screens/SigninScreen";
 
 const Stack = createStackNavigator();
 
@@ -13,7 +14,7 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="Home" component={Onboarding} />
+                <Stack.Screen name="Home" component={SigninScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
--- app/Screens/SigninScreen.js
+++ app/Screens/SigninScreen.js
@@ -1,10 +1,43 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS, images, SIZES } from "../constants";
+import PhoneInput from "react-native-phone-input";
 
 const SigninScreen = () => {
+    const [phoneNumber, setPhoneNumber] = useState(false);
+    const phoneRef = useRef(undefined);
     return (
-        <View>
-            <Text>SigninScreen</Text>
+        <View style={{ backgroundColor: "#FCFCFC", flex: 2 }}>
+            <Image
+                source={images.fruitsBag}
+                // resizeMode={"contain"}
+                style={{ height: SIZES.height / 2, width: "100%" }}
+            />
+            <SafeAreaView style={{ flex: 1 }}>
+                <View
+                    style={{
+                        width: "60%",
+                        marginLeft: "5%",
+                        position: "absolute",
+                        top: 0,
+                    }}
+                >
+                    <Text style={{ fontSize: 25, fontWeight: "bold" }}>
+                        Get your groceries with nectar
+                    </Text>
+                    <TouchableOpacity>
+                        <PhoneInput
+                            style={{
+                                borderColor: "#ddd",
+                                borderWidth: 2,
+                                borderRadius: 2,
+                                padding: 16,
+                            }}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
         </View>
     );
 };
