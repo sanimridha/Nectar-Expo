b680af725e8f4b403e9f7394d6195b76b5afae27
sanimridha <sanimridha307@gmail.com>
2021-06-02 18:22:55 +0600

    imported new images

--- app/constants/images.js
+++ app/constants/images.js
@@ -12,6 +12,14 @@ const Explore = require("../../assets/photos/Explore.png");
 const Cart = require("../../assets/photos/Cart.png");
 const Shop = require("../../assets/photos/Shop.png");
 const Person = require("../../assets/photos/Person.png");
+const banner = require("../../assets/photos/banner.png");
+const banner2 = require("../../assets/photos/banner2.jpg");
+const banner3 = require("../../assets/photos/banner3.jpg");
+const apple = require("../../assets/photos/Product/apple.png");
+const banana = require("../../assets/photos/Product/banana.png");
+const Beef = require("../../assets/photos/Product/Beef.png");
+const Cupsicum = require("../../assets/photos/Product/Cupsicum.png");
+const potato = require("../../assets/photos/Product/potato.png");
 
 export default {
     accepted,
@@ -28,4 +36,12 @@ export default {
     Cart,
     Shop,
     Person,
+    banner,
+    banner2,
+    banner3,
+    apple,
+    banana,
+    Beef,
+    Cupsicum,
+    potato,
 };
