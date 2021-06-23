0755229cd27e1c63e78b385ccf2b5a4b700d7959
Sani Mridha <sanimridha307@gmail.com>
2021-06-23 10:13:07 +0600

    checking user isLogged in

--- app/navigation/Routes.js
+++ app/navigation/Routes.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, View } from 'react-native';
-import AuthStack from './AuthStack';
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import AuthStack from "./AuthStack";
+import AppStack from "./AppStack";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Routes = () => {
-  return (
-    <NavigationContainer>
-      <AuthStack />
-    </NavigationContainer>
-  );
+    const [initialRoute, setinitialRoute] = useState(false);
+    useEffect(() => {
+        getUserCredentials();
+    }, []);
+    const getUserCredentials = async () => {
+        try {
+            const value = await AsyncStorage.getItem("userinfo");
+            const data = JSON.parse(value);
+            if (data.data.token) {
+                setinitialRoute(true);
+            }
+        } catch (error) {
+            // Error retrieving data
+            console.log(
+                "This error from token issue means user is not available !" +
+                    error
+            );
+        }
+    };
+    return (
+        <NavigationContainer>
+            {initialRoute ? <AppStack /> : <AuthStack />}
+        </NavigationContainer>
+    );
 };
 
 export default Routes;
